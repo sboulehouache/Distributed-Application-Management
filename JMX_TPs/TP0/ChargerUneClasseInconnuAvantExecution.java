b054c2cb2a38ddfd10051e75cee7cd9d2f8bb5a9
Boulehouache <sboulehouache@yahoo.com>
2021-03-19 22:28:50 +0100

    correction

--- JMX_TPs/TP0/ChargerUneClasseInconnuAvantExecution.java
+++ JMX_TPs/TP0/ChargerUneClasseInconnuAvantExecution.java
@@ -8,6 +8,7 @@
  * @author souf
  */
 import javax.swing.JTextField;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollBar;
@@ -16,119 +17,100 @@ import javax.swing.JTextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ChargerUneClasseInconnuAvantExecution extends JFrame implements ActionListener {
-JTextArea text= new JTextArea();
-JScrollBar scrollBar=new JScrollBar();
-JButton ok=new JButton("ok");
-JButton increaseSize=new JButton("Increase Size");
-int i;
-int j;
-String s;
+    JTextArea text = new JTextArea();
+    JScrollBar scrollBar = new JScrollBar();
+    JButton btnOpenClass = new JButton("ok");
+    JButton increaseSize = new JButton("Increase Size");
+    int i;
+    int j;
+    String s;
+    File classfile;
+    Class className = null;
+    Method meth = null;
+    Object objectName = null;
 
-Class c=null;
-Method meth=null;
-Object name=null;
-public ChargerUneClasseInconnuAvantExecution(){
-    this.setLayout(null);
-    this.setSize(400, 400);
-    this.setLocation(30,400);
-    
-    ok.setSize(150,30);
-    ok.setLocation(20, 20);
-    
-    increaseSize.setSize(150,30);
-    increaseSize.setLocation(20, 120);
-    
-    
-    scrollBar.add(text);
-    scrollBar.setSize(200,400);
-    scrollBar.setLocation(20, 60);
-    add(text);
-    add(ok);
-    add(increaseSize);
-    this.setVisible(true);
-    ok.addActionListener(new ActionListener() {
+    public ChargerUneClasseInconnuAvantExecution() {
+	this.setLayout(null);
+	this.setSize(400, 400);
+	this.setLocation(30, 400);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	    // TODO Auto-generated method stub
-	    
-	}});
-    increaseSize.addActionListener(this);
-    /*increaseSize.addActionListener(new ActionListener() {
+	btnOpenClass.setSize(150, 30);
+	btnOpenClass.setLocation(20, 20);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	    
-	    try {
-		Class<?>[] paramTypes = {int.class};
-		meth = c.getMethod("increaseSize", paramTypes);
-		Object resultat = meth.invoke(name, 10);
-	    } catch (IllegalAccessException e1) {
-		e1.printStackTrace();
-	    } catch (IllegalArgumentException e1) {
-		e1.printStackTrace();
-	    } catch (InvocationTargetException e1) {
-		e1.printStackTrace();
-	    } catch (NoSuchMethodException e1) {
-		e1.printStackTrace();
-	    } catch (SecurityException e1) {
-		e1.printStackTrace();
+	increaseSize.setSize(150, 30);
+	increaseSize.setLocation(20, 120);
+
+	scrollBar.add(text);
+	scrollBar.setSize(200, 400);
+	scrollBar.setLocation(20, 60);
+	add(text);
+	add(btnOpenClass);
+	add(increaseSize);
+	this.setVisible(true);
+
+	increaseSize.addActionListener(this);
+
+	JButton btnOpenClass = new JButton("Open Class");
+	btnOpenClass.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent arg0) {
+		ClassOpener co = new ClassOpener();
+		classfile = co.file;
+		String classFileName = classfile.getName().substring(0, classfile.getName().length() - 5);
+		try {
+		    className = Class.forName(classFileName);
+		    objectName = className.newInstance();
+		} catch (ClassNotFoundException e) {
+		    e.printStackTrace();
+		} catch (InstantiationException e) {
+		    e.printStackTrace();
+		} catch (IllegalAccessException e) {
+		    e.printStackTrace();
+		}
 	    }
-	}
-	
-    });*/
-}
+	});
+    }
 
-public static void main(String arg[]){
-        //Class c=ChargerUneClasseInconnuAvantExecution.class;
-    
-    new ChargerUneClasseInconnuAvantExecution();
+    public static void main(String arg[]) {
+	// Class c=ChargerUneClasseInconnuAvantExecution.class;
 
-}
-@Override
-public void actionPerformed(ActionEvent ae) {
-    Object source = ae.getSource();
-    try {
-	    String nomClass="classestobecharged.AClass";
-	    if(c==null) {
-	    c=Class.forName(nomClass);
-	    name=c.newInstance();
-	    }    
-	    if(source==ok) {
-		System.out.println("ok");
-	    meth = c.getMethod("start"); 
-	    ok.setText(meth.getName());
-	    Object resultat = meth.invoke(name);
-	    }   
-	    if(source==increaseSize) {
+	new ChargerUneClasseInconnuAvantExecution();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+	Object source = ae.getSource();
+	try {
+
+	    if (source == increaseSize) {
 		System.out.println("increaseSize");
-	    Class<?>[] paramTypes = {String.class,int.class};
-	    meth = c.getMethod("increaseSize", paramTypes);
-	    Object resultat = meth.invoke(name, "hello", 50);
+		Class<?>[] paramTypes = { String.class, int.class };
+		meth = className.getMethod("increaseSize", paramTypes);
+		Object resultat = meth.invoke(objectName, "hello", 50);
 	    }
-	   	    
-	    text.setText("h "+name.toString());
-	    
-	    Class[] interfaces = c.getInterfaces();
-	    Class[] classes = c.getClasses();
-	    Method[] methods= c.getMethods();
-	    Field[] fields=c.getFields();
-	    Field[] delaredFields=c.getDeclaredFields();
-	    Class superClasses=c.getSuperclass();
-	    for(int i = 0; i < delaredFields.length; i++)
-	            System.out.println(delaredFields[i]);
-	}
-    	catch (InvocationTargetException e) {
+
+	    text.setText("h " + objectName.toString());
+
+	    Class[] interfaces = className.getInterfaces();
+	    Class[] classes = className.getClasses();
+	    Method[] methods = className.getMethods();
+	    Field[] fields = className.getFields();
+	    Field[] delaredFields = className.getDeclaredFields();
+	    Class superClasses = className.getSuperclass();
+	    for (int i = 0; i < delaredFields.length; i++)
+		System.out.println(delaredFields[i]);
+	} catch (InvocationTargetException e) {
 	    e.getCause().printStackTrace();
-    	}
-    	catch (Exception e){text.setText("Exception");
-	e.printStackTrace();
+	} catch (Exception e) {
+	    text.setText("Exception");
+	    e.printStackTrace();
 	}
-    
-}
+
+    }
 }
