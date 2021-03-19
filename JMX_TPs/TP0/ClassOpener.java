3a41dba656b730505f47c6c8a91694ca58ff61f2
Boulehouache <sboulehouache@yahoo.com>
2021-03-19 22:32:04 +0100

    correction

---
+++ JMX_TPs/TP0/ClassOpener.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+public class ClassOpener {
+	File file;
+
+	public ClassOpener(){
+		JFileChooser fileChooser=new  JFileChooser();
+		fileChooser.setVisible(true);
+		fileChooser.setCurrentDirectory(new File("E:\\EclipseWorkSpace\\JMX_TPs\\TP0\\src"));
+		if (fileChooser.showOpenDialog(null)==JFileChooser.APPROVE_OPTION) {
+			file=fileChooser.getSelectedFile();
+		}
+	}
+
+	public File getFile() {
+	    return file;
+	}
+}
